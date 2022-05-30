99aac4cfc16196ad3db3e0a9a0c813f24d50f869
huayan <2503008111@qq.com>
2022-05-30 20:36:45 +0800

    添加了index信息

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+function getUserInfo(){
+    $.ajax({
+        type:'get',
+        url:'/my/userinfo',
+        // headers:{
+        //     Authorization:localStorage.getItem('token')
+        // },
+        success:function(res){
+            if(res.status!==0)return layer.msg('获取用户信息失败')
+            layer.msg('获取用户信息成功')
+            renderAvater(res.data)
+        }
+    })
+}
+const renderAvater=(user)=>{
+    console.log(user);
+    let uname=user.nickname||user.username
+    $('#welcome').html(`欢迎${uname}`)
+    if(user.user_pic!==null){
+       $('.layui-nav-img').attr('src',user.user_pic)
+       $('.text-avatar').hide() 
+    }else{
+        $('.text-avatar').html(uname[0].toUpperCase())
+        $('.layui-nav-img').hide()
+    }
+}
+$('#btnlogout').click(function(){
+    layui.layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "" },
+        function (index) {
+            // 清空本地存储里面的 token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "/login.html";
+        }
+    );
+})
+getUserInfo()
